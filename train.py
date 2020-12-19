9318d0b6e1f1180f9ffb9d077be2830a5869af18
Zexi <zexi@zexi.me>
2020-12-20 07:39:42 +0800

    :sparkles:

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 dataset = MyDataset()
 
-batch_size = 5
+batch_size = 30
 learning_rate = 1e-4
 num_epochs = 50
 num_classes = dataset.category_num
